ea774d42616519efb329b0713bc0cfb16050db24
Ayaan31 <ahmada5@rpi.edu>
2024-10-18 16:05:18 -0400

    Added endpoints to make

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,27 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello TypeScript with Express!');
 });
 
+// Retrieve a zipped folder for the assigment associated with {key} 
+app.get('/assignment/{key}', (req: Request, res: Response) => {
+  res.send('Hello TypeScript with Express!');
+});
+
+// Add assignment information with the associated key 
+app.post('/assignment/{key}', (req: Request, res: Response) => {
+  res.send('Hello TypeScript with Express!');
+});
+
+app.put('/assignment/{key}', (req: Request, res: Response) => {
+  res.send('Hello TypeScript with Express!');
+});
+
+// Delete the assignment information associated with {key}
+app.delete('/assignment/{key}', (req: Request, res: Response) => {
+  res.send('Hello TypeScript with Express!');
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
